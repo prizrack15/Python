b2ad575f21012ea47bc3435823fa261f83304f8c
prizrack15 <71446043+prizrack15@users.noreply.github.com>
2023-04-02 18:23:44 +0300

    Add files via upload

---
+++ Selection_sort.py
@@ -0,0 +1,22 @@
+def find_Smallest(arr): # Нахождение наименьшего символа
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    smallest = ()
+    for i in range(len(arr)):
+        smallest = arr.index(min(arr))
+        print(smallest)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+c = [int(i) for i in input().split()]
+
+print(c[find_Smallest(c)], 'kek')
+print(selectionSort(c))
